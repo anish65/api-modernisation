95739fc8bd84c993aa1ceb20bf07ac926fc1b6e1
Anish A <aniyanveetil@gmail.com>
2023-10-05 21:58:02 +0530

    Configuring legacy to serve 3 TPS

--- legacy-service/src/main/java/com/example/legacyservice/controller/TransactionController.java
+++ legacy-service/src/main/java/com/example/legacyservice/controller/TransactionController.java
@@ -2,21 +2,40 @@ package com.example.legacyservice.controller;
 
 import com.zand.system.common.messaging.kafka.message.FundTransferRQMessage;
 import com.zand.system.common.messaging.kafka.message.FundTransferRSMessage;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
 @RestController
 @Slf4j
 public class TransactionController {
 
+    private Bucket bucket;
+
+    @PostConstruct
+    public void init() {
+        Bandwidth limit = Bandwidth.classic(3, Refill.greedy(3, Duration.ofMinutes(1)));
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+
     @PostMapping("/fundTransfer")
     public ResponseEntity<FundTransferRSMessage> doFundTransfer(@RequestBody FundTransferRQMessage message) throws InterruptedException {
+        if (!bucket.tryConsume(1)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         log.info("Received message with key: {}, message: {}", message.getReferenceNo(), message);
-        Thread.sleep(8000l);
         FundTransferRSMessage fundTransferRSMessage = new FundTransferRSMessage();
         fundTransferRSMessage.setReferenceNo(message.getReferenceNo());
         fundTransferRSMessage.setCifId(message.getCifId());
